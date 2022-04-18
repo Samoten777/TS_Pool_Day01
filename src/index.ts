5ac7f0c3f4a6e66d35861e8afa3b63b21a901d77
Laure GAGNER <laure.gagner@epitech.eu>
2022-04-18 15:53:09 -0400

    feat : helloName

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import {getSortedEvenNumbers} from "./getSortedEvenNumbers";
+import {helloName} from "./helloName";
 
-const test = [1, 2, 12, 3, 4, 10, 5, 6, 7, 8];
 
-console.log(getSortedEvenNumbers(test));
+helloName();
